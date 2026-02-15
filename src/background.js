48ce89567b2f2bc3c7e606b989e91e1a40029494
Ulrik Lyngs <ulrik.lyngs@gmail.com>
2026-02-15 10:19:23 +0000

    make sure clicking the extension icon also opens it on chrome

--- src/background.js
+++ src/background.js
@@ -4,17 +4,20 @@
  * Opens (or focuses) popup.html in a tab when the extension icon is clicked.
  */
 
-browser.action.onClicked.addListener(async () => {
+// Normalise the browser API global (Firefox = `browser`, Chrome/Edge = `chrome`)
+const api = typeof browser !== 'undefined' ? browser : chrome;
+
+api.action.onClicked.addListener(async () => {
     // Check if popup.html is already open in a tab
-    const extensionUrl = browser.runtime.getURL('popup.html');
-    const tabs = await browser.tabs.query({ url: extensionUrl });
+    const extensionUrl = api.runtime.getURL('popup.html');
+    const tabs = await api.tabs.query({ url: extensionUrl });
 
     if (tabs.length > 0) {
         // Focus existing tab
-        await browser.tabs.update(tabs[0].id, { active: true });
-        await browser.windows.update(tabs[0].windowId, { focused: true });
+        await api.tabs.update(tabs[0].id, { active: true });
+        await api.windows.update(tabs[0].windowId, { focused: true });
     } else {
         // Open new tab
-        await browser.tabs.create({ url: extensionUrl });
+        await api.tabs.create({ url: extensionUrl });
     }
 });
